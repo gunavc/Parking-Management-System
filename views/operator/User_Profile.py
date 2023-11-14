16ce28e1370f0a96a8142368343ad2a7797624eb
gunavc <97451210+gunavc@users.noreply.github.com>
2023-11-14 17:00:18 +0530

    Added all CUD operations to operators

--- views/operator/User_Profile.py
+++ views/operator/User_Profile.py
@@ -10,7 +10,7 @@ def get_name(id):
             database="parking_system"
         )
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM parking_system.employees WHERE empid=%s", (id, ))
+        cursor.execute("SELECT * FROM parking_system.employees WHERE empid=%s", (id))
         data = cursor.fetchone()
 
         conn.commit()
@@ -18,6 +18,41 @@ def get_name(id):
         conn.close()
 
         return data
+    except Exception as e:
+        st.error(e)
+
+def update_name(fname, lname, id):
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="proj",
+            password="proj",
+            database="parking_system"
+        )
+        cursor = conn.cursor()
+        cursor.execute("UPDATE employees SET fname=%s WHERE empid=%s", (fname, id))
+        cursor.execute("UPDATE employees SET lname=%s WHERE empid=%s", (lname, id))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        st.error(e)
+
+def update_address(addr, id):
+    try:
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="proj",
+            password="proj",
+            database="parking_system"
+        )
+        cursor = conn.cursor()
+        cursor.execute("UPDATE employees SET address=%s WHERE empid=%s", (addr, id))
+
+        conn.commit()
+        cursor.close()
+        conn.close()
     except Exception as e:
         st.error(e)
 
@@ -30,19 +65,13 @@ def create_page(id):
     # Sample User Profile generated, Have to retreive this from the database
     st.title("User Profile")
     st.divider()
-    user_details = {
-        "fname":"John",
-        "lname":"Doe",
-        "emp_no":"PES1UG21CS684",
-        "lot_no":"0",
-        "addr":"timbuktu"
-    }
+    user_details = get_name(id)
 
     # name
     st.header("Name")
     name_val, name_stopgap, name_edit = st.columns(3, gap="large")
     with name_val:
-        st.subheader(user_details["fname"]+" "+user_details["lname"])
+        st.subheader(user_details[0]+" "+user_details[1])
     with name_edit:
         if st.button(label="Edit Name", key=0):
             name_change = True
@@ -50,29 +79,36 @@ def create_page(id):
             with name_val:
                 new_name = str(st.text_input(
                     label="Enter Your New Name", 
-                    value=user_details["fname"] + " " +user_details["lname"],
+                    value=user_details[0] + " " +user_details[1],
                     placeholder="First_Name Last_Name"))
                 new__name = new_name.split(" ")
+                update_name(new_name[0], new_name[1], id)
+
+    st.divider()
+
+    # role (not changable by employee)
+    st.header("Role")
+    st.subheader(user_details[3])
 
     st.divider()
 
     # emp num (not changable by employee)
     st.header("Employee Number")
-    st.subheader(user_details["emp_no"])
+    st.subheader(user_details[2])
 
     st.divider()
 
     # lot number (not changable by employee)
     st.header("Lot Number")
-    st.subheader(user_details["lot_no"])
+    st.subheader(user_details[4])
 
-    st.divider()
+    # st.divider()
 
     # address
     st.header("Address")
     addr_val, addr_stopgap, addr_edit = st.columns(3, gap="large")
     with addr_val:
-        st.subheader(user_details["addr"])
+        st.subheader(user_details[5])
     
     with addr_edit:
         if st.button(label="Edit Address", key=1):
@@ -81,6 +117,6 @@ def create_page(id):
             with addr_val:
                 new_addr = str(st.text_input(
                     label="Enter Your New Address", 
-                    value=user_details["addr"],
+                    value=user_details[5],
                     placeholder="Street, Locality, City, Pincode"))
-        
+                update_address(new_addr, id)
